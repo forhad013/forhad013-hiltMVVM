8375ead5e9913504c78c2dac14efb38bd16be5cf
forhad013 <forhad013@gmail.com>
2022-01-07 21:48:22 +0600

    git added

---
+++ app/src/main/java/com/invertemo/june_app_mvvm/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.invertemo.june_app_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
