f01bee7e8e9938865331f0413b74b7b1eb3bdc4f
forhad013 <forhad013@gmail.com>
2022-01-07 23:05:31 +0600

    init

---
+++ app/src/main/java/com/invertemo/june_app_mvvm/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.invertemo.june_app_mvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//inital commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
