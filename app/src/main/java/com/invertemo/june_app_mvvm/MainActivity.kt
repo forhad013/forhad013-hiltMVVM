c22ebb06cdc416499ad8d5eb91330f2e11feb7b9
forhad013 <forhad013@gmail.com>
2022-01-07 22:09:11 +0600

    second comment on main

--- app/src/main/java/com/invertemo/june_app_mvvm/MainActivity.kt
+++ app/src/main/java/com/invertemo/june_app_mvvm/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //initial comment
+//main comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
